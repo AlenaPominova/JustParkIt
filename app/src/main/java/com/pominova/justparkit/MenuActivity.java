1bbca7f8752ea2435e11fbc3d269bbf3186be798
Alena Pominova <alena.pominova@gmail.com>
2020-05-03 18:59:50 +0300

    Change theme

--- app/src/main/java/com/pominova/justparkit/MenuActivity.java
+++ app/src/main/java/com/pominova/justparkit/MenuActivity.java
@@ -1,5 +1,7 @@
 package com.pominova.justparkit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.navigation.NavController;
@@ -15,15 +17,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.View;
+import android.widget.Switch;
 
 public class MenuActivity extends AppCompatActivity {
 
+    public static final String APP_PREFERENCES_THEME = "theme";
+    public static final String IS_DARK_THEME = "isDarkTheme";
+    private SharedPreferences settings;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        loadSettings();
+        loadTheme();
+
         setContentView(R.layout.activity_menu);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -52,5 +64,49 @@ public class MenuActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void ToggleTheme(boolean isChecked){
+        if (isChecked) {
+            this.getTheme().applyStyle(R.style.AppDarkTheme, true);
+        }
+        else{
+            this.getTheme().applyStyle(R.style.AppTheme, true);
+        }
+    }
+
+    public void switchTheme(View view) {
+        boolean isChecked = ((Switch)findViewById(R.id.darkThemeSwitch)).isChecked();
+
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(IS_DARK_THEME, isChecked);
+        editor.apply();
+        editor.commit();
+
+        reloadTheme();
+    }
+
+    public SharedPreferences getSettings() {
+        return settings;
+    }
+
+    private void loadSettings() {
+        settings = this.getSharedPreferences(APP_PREFERENCES_THEME, Context.MODE_PRIVATE);
+    }
+
+    private void loadTheme() {
+        int theme = getThemeFromPreferences();
+        setTheme(theme);
+    }
+
+    private void reloadTheme() {
+        int theme = getThemeFromPreferences();
+        setTheme(theme);
+        MenuActivity.this.recreate();
+    }
+
+    private int getThemeFromPreferences() {
+        Boolean darkTheme = settings.getBoolean(IS_DARK_THEME, false);
+        return darkTheme ? R.style.AppDarkTheme : R.style.AppTheme;
     }
 }
